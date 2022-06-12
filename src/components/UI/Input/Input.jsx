ca5eed4eb779352767b73d3aa99bd3d8af088bfd
hoppo97 <hoppo97@bk.ru>
2022-06-12 21:57:55 +0300

    Слегка переделал структуру компонентов

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './input.module.scss';
 
-export const Input = ({value, setNameTodo, blur, placeholder = 'Введите название задачи'}) => {
+export const Input = React.memo(({value, setNameTodo, blur, placeholder = 'Введите название задачи'}) => {
   return (
       <input className={styles.input} placeholder={placeholder} value={value} onBlur={blur} onChange={setNameTodo} type="text" />
   )
-}
+}) 
 
 
